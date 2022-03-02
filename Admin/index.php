61f1a076b32b8a524ab6286b910a36d0951a2c72
CCHeng730 <chinheng3007@gmail.com>
2022-03-02 20:23:31 +0800

    Layout

---
+++ Admin/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <base href="../">
+      <meta charset="utf-8" />
+      <title>PriceCom | Admin</title>
+      <link href="assets/app.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+      <?php include "layout/header.php" ?>
+      <div>Pundek Cock</div>
+    </body>
+</html>
---
+++ Admin/layout/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <base href="../">
+      <meta charset="utf-8" />
+      <title>PriceCom | Admin</title>
+      <link href="assets/app.css" rel="stylesheet" type="text/css" />
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </head>
+    <body>
+      <div class="tw-text-blue-500">Pundek Hang</div>
+    </body>
+</html>
